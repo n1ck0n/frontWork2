a4031c1c6061666a9bd9a7759a7a3a4c21dfe7d8
n1ck0n <novozeninnikita2@gmail.com>
2025-02-20 15:09:01 +0300

    The 404 error output has been fixed

--- server.js
+++ server.js
@@ -3,20 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'index.html');
-
-    if (req.url === '/styles.css') {
-        filePath = path.join(__dirname, 'styles.css');
-    }
-
-    fs.readFile(filePath, (err, content) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<h1>404 - Страница не найдена</h1>');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
+            res.end('<h1>404 error (oshibka)</h1>');
+            return;
         }
+        let contentType = 'text/html';
+        if (filePath.endsWith('.css')) {
+            contentType = 'text/css';
+        }
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 - Внутренняя ошибка сервера</h1>');
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content);
+            }
+        });
     });
 });
 
